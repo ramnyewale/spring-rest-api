757811797a9b7d943f88fb9151720c4549dc99df
ram yewale <ramn.yewale@gmail.com>
2022-09-16 09:44:32 +0530

    Swagger documentation

--- src/main/java/com/spring/rest/api/SpringRestApiApplication.java
+++ src/main/java/com/spring/rest/api/SpringRestApiApplication.java
@@ -1,9 +1,16 @@
 package com.spring.rest.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableAutoConfiguration
+@EnableWebMvc
 public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
